3aba70843cbe5fe83db9bf1407f411c1fd38610e
Test User <Test5User@yandex.by>
2023-10-09 21:18:18 +0300

    Revert "Second commit"
    This reverts commit 032d0d5eae4ab42e1d44f231285f8d8d7c5f1561.

--- src/by/itclass/Main.java
+++ src/by/itclass/Main.java
@@ -3,6 +3,5 @@ package by.itclass;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
-        System.out.println("Other changes/");
     }
 }
